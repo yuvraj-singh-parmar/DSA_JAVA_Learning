95bbf84eae4635b7ea87883cf360eb67c1d44f2d
yuvraj-singh-parmar <yuvip946@gmail.com>
2026-08-21 21:56:19 +0530

    New Patterns Added

---
+++ 01_STAR_PATTERNS/pattern6.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+/*
+
+*********
+*******
+*****
+***
+*
+
+*/
+
+class pattern6{
+    public static void main(String args[]){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1; j<= (n-i) + (n-i) + 1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
